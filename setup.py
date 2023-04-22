664309eb402115ad45babf5af1af40d411f4fa75
__coelho__ <109885900+MikalROn@users.noreply.github.com>
2023-04-22 03:17:22 -0300

    Ajustando para distribuição

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 
-with open('README.md', 'rt', encoding='latin-1') as arq:
+with open('README.md', 'rt', encoding='utf-8') as arq:
       readme = arq.read()
 
-keywords = ['Api omie', 'omie', 'api do omie', 'omieapi']
+keywords = ['pychat']
 
-setup(name='api-omie',
-      url='',
+setup(name='cli-chat',
+      url='https://github.com/MikalROn/WS-CLI-Chat',
       version='0.1.0',
       license='MIT license',
       author='Daniel Coêlho',
@@ -15,7 +15,7 @@ setup(name='api-omie',
       long_description_content_type='text/markdown',
       author_email='heromon.9010@gmail.com',
       keywords=keywords,
-      description='Ferramenta para api do omie não oficial',
+      description='simple cli-websocket-chat',
       packages=['chat'],
       install_requires=['websockets'],
       python_requires='>=3',
